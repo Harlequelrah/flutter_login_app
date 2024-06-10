caf478b6be2d60fe43c555155038766416090a26
Harlequelrah <degbovimax19@gmail.com>
2024-06-10 01:01:38 +0000

    le champ password

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
   final String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -66,45 +66,99 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  padding: const EdgeInsets.only(top: 10.0, bottom: 40.0),
                   child: Text(
                     "BIENVENUE SUR NOTRE PAGE DE CONNEXION",
                     style: TextStyle(
                       fontSize: 20,
                       color: Colors.amber,
                       fontWeight: FontWeight.bold,
-                      background: Paint()..color = Theme.of(context).colorScheme.primary,
+                      background: Paint()
+                        ..color = Theme.of(context).colorScheme.primary,
                     ),
                   ),
                 ),
                 Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(children: [
-                      Icon(
-                        Icons.account_circle_outlined,
-                        size: 55,
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: SizedBox(
-                            height: 50,
-                            child: TextField(
-                              style: TextStyle(color:Theme.of(context).colorScheme.primary),
-                              decoration: InputDecoration(
-                                labelText: "Email",
-                                // labelStyle: TextStyle(color:Theme.of(context).colorScheme.secondary),
-                                hintText: "Entrer votre email (e.g:usernames@example.com)",
-                                // hintStyle: TextStyle(color:Colors.purple),
-                                 filled: true, // Activer le remplissage du champ
-                                fillColor: Theme.of(context).colorScheme.inversePrimary,
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(color: Colors.amber),
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.account_circle_outlined,
+                            size: 55,
+                          ),
+                          const SizedBox(width: 10),
+                          SizedBox(
+                              height: 50,
+                              width: 400,
+                              child: TextField(
+                                style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary),
+                                decoration: InputDecoration(
+                                  labelText: "Email",
+                                  // labelStyle: TextStyle(color:Theme.of(context).colorScheme.secondary),
+                                  hintText:
+                                      "Entrer votre email (e.g:usernames@example.com)",
+                                  // hintStyle: TextStyle(color:Colors.purple),
+                                  filled:
+                                      true, // Activer le remplissage du champ
+                                  fillColor: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary,
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide:
+                                        const BorderSide(color: Colors.amber),
+                                  ),
                                 ),
-                              ),
-                            )),
-                      )
-                    ])),
+                              )),
+                        ])),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.vpn_key, size: 35),
+                        const Icon(Icons.lock, size: 35),
+                        const SizedBox(width: 5),
+                        SizedBox(
+                            height: 50,
+                            width: 400,
+                            child: TextField(
+                                obscureText: isObscure,
+                                autocorrect: false,
+                                enableSuggestions: false,
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: InputDecoration(
+                                  labelText: "Mot de passe",
+                                  hintText: "Entrer votre mot de passe",
+                                  suffixIcon: IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        isObscure = !isObscure;
+                                      });
+                                    },
+                                    icon: isObscure
+                                        ? const Icon(Icons.visibility)
+                                        : const Icon(Icons.visibility_off),
+                                  ),
+                                  filled:
+                                      true, // Activer le remplissage du champ
+                                  fillColor: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary,
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: const BorderSide(
+                                        color: Color.fromRGBO(255, 193, 7, 1)),
+                                  ),
+                                )))
+                      ],
+                    ))
               ],
             ),
           ),
